378abc575dc118eeb6232600489d74637c2fa2cf
Yure Moutella <ymoutella@gmail.com>
2025-12-28 21:59:53 -0300

    First 2d projects finished

--- main.gd
+++ main.gd
@@ -4,15 +4,20 @@ extends Node
 var score
 
 func _ready() -> void:
-	new_game()
-
+	get_tree().call_group("mobs", "queue_free")
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 func new_game() -> void:
 	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$Music.play()
 
 
 func _on_mob_timer_timeout() -> void:
@@ -35,6 +40,7 @@ func _on_mob_timer_timeout() -> void:
 	
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
